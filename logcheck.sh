2e09a8f5905954d39bbe5772a87553b3cb379d98
ajinsusumu <ajinsusumu@users.noreply.github.com>
2017-02-06 16:55:30 +0800

    add comments

--- logcheck.sh
+++ logcheck.sh
@@ -3,6 +3,9 @@
 # get logs from repo
 (
 cd ~/sm10work
+# -p for printing the project names
+# --date=iso for printing user friendly dates
+# --stat for printing the file changes
 ./repo forall \
 -p \
 -c git log --since="2017-1-1" --until="2017-2-6" \
